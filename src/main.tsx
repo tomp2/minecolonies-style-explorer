5db90dbc226f0773d092897f271128e9a5b957e8
tomp2 <51736857+tomp2@users.noreply.github.com>
2024-12-17 04:43:11 +0200

    feat: cookieless analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
+import { type PostHogConfig } from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+const options: Partial<PostHogConfig> = {
+    api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+    persistence: "memory",
+};
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <PostHogProvider apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY} options={options}>
+            <App />
+        </PostHogProvider>
     </StrictMode>,
 );
