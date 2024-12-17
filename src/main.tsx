7fd1e0374d89b5930dd15ec076c62ab40896e811
tomp2 <51736857+tomp2@users.noreply.github.com>
2024-12-17 04:55:43 +0200

    fix: wrong posthog host env key

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 const options: Partial<PostHogConfig> = {
-    api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+    api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
     persistence: "memory",
 };
 
