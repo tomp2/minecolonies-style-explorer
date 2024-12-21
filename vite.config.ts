4a3e970eb17a48df0eeb86e27a17aa96a1f5dd2e
tomp2 <51736857+tomp2@users.noreply.github.com>
2024-12-22 00:24:31 +0200

    build: sitemap + robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-    plugins: [react(), Sitemap({ hostname: "https://tomp2.github.io/minecolonies-style-explorer/" })],
+    plugins: [react()],
     base: process.env.NODE_ENV === "production" ? "/minecolonies-style-explorer/" : "/",
     resolve: {
         alias: {
