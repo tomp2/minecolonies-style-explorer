167c5a7e9676d81cdc0104895018c6eb3fe95b0e
tomp2 <51736857+tomp2@users.noreply.github.com>
2024-12-25 14:20:56 +0200

    refactor: better logic for main content page

--- src/ui/page-content.tsx
+++ src/ui/page-content.tsx
@@ -3,7 +3,6 @@ import {
     favoritePaths,
     pageContentAtom,
     searchTermAtom,
-    selectedThemesAtom,
     showFavoritesAtom,
 } from "@/lib/state-atoms.ts";
 import { type BuildingData } from "@/lib/theme-data.ts";
@@ -70,11 +69,14 @@ function BuildingsContainer() {
 }
 
 export function PageContent() {
-    const selectedThemes = useAtomValue(selectedThemesAtom);
     const searchTerm = useAtomValue(searchTermAtom);
     const showFavorites = useAtomValue(showFavoritesAtom);
     const favoriteCount = useAtomValue(favoritePaths).length;
     const { totalBuildingsFound } = useAtomValue(pageContentAtom);
+
+    if (totalBuildingsFound > 0 || (showFavorites && favoriteCount > 0)) {
+        return <BuildingsContainer />;
+    }
 
     if (searchTerm && totalBuildingsFound === 0) {
         return (
@@ -89,59 +91,55 @@ export function PageContent() {
             </article>
         );
     }
-    if (selectedThemes.size === 0 && (!showFavorites || favoriteCount == 0)) {
-        return (
-            <article className="prose prose-xl mx-auto mt-5 pb-14">
-                <h1 className="text-4xl font-extrabold">
-                    Welcome to the <em>unofficial</em> MineColonies Style Explorer!
-                </h1>
-                <p>
-                    This is an <em>unofficial</em> site for browsing screenshots of the{" "}
-                    <a className="text-blue-500" href="https://minecolonies.com/">
-                        MineColonies
-                    </a>{" "}
-                    mod theme/style buildings. <b>Credits</b> for the buildings go to the style authors listed
-                    in the sidebar next to the style name. I created this site to make it easy to visually
-                    browse the buildings from different styles and categories at the same time.
-                </p>
-                <p>
-                    To get started, select one or more styles/categories from the sidebar and start exploring
-                    the buildings! You can also search for a building name, hut block, or category—just select
-                    all the styles you want to search from first. You can see the back of most buildings by
-                    clicking on the image.
-                </p>
-                <p>
-                    Note: This page only shows simple pics of the buildings. Minecolonies has a Patreon where
-                    you can support them and get access to their official <b>schematics server</b> if you want
-                    to browse and inspect the buildings more closely in-game. Read more about it on their{" "}
-                    <a className="text-blue-500" href="https://www.patreon.com/minecolonies">
-                        Patreon page
-                    </a>
-                </p>
-                <p className="mb-0">
-                    Some of the styles, categories, or buildings may be
-                    <strong> missing or excluded</strong> for reasons like:
-                </p>
-                <ul className="mt-0">
-                    <li>I haven&#39;t used the style in my own worlds yet.</li>
-                    <li>The builds have been updated and I have not taken new screenshots.</li>
-                    <li>
-                        Some styles have a lot off little pieces that I decided not to include for now to save
-                        space and time (mostly roads, mineshafts and some infrastructure).
-                    </li>
-                    <li>New buildings have been added, or I have forgotten some.</li>
-                </ul>
-                <p>
-                    Send me some feedback (button on top right) if you notice changed or missing buildings!
-                    I&#39;ll add styles here if people request them and I have time :)
-                </p>
-                <p>
-                    Viewing buildings at different levels is not supported yet, but I might add it in the
-                    future. Currently the screenshots are of the maximum level of the building.
-                </p>
-            </article>
-        );
-    }
-
-    return <BuildingsContainer />;
+    return (
+        <article className="prose prose-xl mx-auto mt-5 pb-14">
+            <h1 className="text-4xl font-extrabold">
+                Welcome to the <em>unofficial</em> MineColonies Style Explorer!
+            </h1>
+            <p>
+                This is an <em>unofficial</em> site for browsing screenshots of the{" "}
+                <a className="text-blue-500" href="https://minecolonies.com/">
+                    MineColonies
+                </a>{" "}
+                mod theme/style buildings. <b>Credits</b> for the buildings go to the style authors listed in
+                the sidebar next to the style name. I created this site to make it easy to visually browse the
+                buildings from different styles and categories at the same time.
+            </p>
+            <p>
+                To get started, select one or more styles/categories from the sidebar and start exploring the
+                buildings! You can also search for a building name, hut block, or category—just select all the
+                styles you want to search from first. You can see the back of most buildings by clicking on
+                the image.
+            </p>
+            <p>
+                Note: This page only shows simple pics of the buildings. Minecolonies has a Patreon where you
+                can support them and get access to their official <b>schematics server</b> if you want to
+                browse and inspect the buildings more closely in-game. Read more about it on their{" "}
+                <a className="text-blue-500" href="https://www.patreon.com/minecolonies">
+                    Patreon page
+                </a>
+            </p>
+            <p className="mb-0">
+                Some of the styles, categories, or buildings may be
+                <strong> missing or excluded</strong> for reasons like:
+            </p>
+            <ul className="mt-0">
+                <li>I haven&#39;t used the style in my own worlds yet.</li>
+                <li>The builds have been updated and I have not taken new screenshots.</li>
+                <li>
+                    Some styles have a lot off little pieces that I decided not to include for now to save
+                    space and time (mostly roads, mineshafts and some infrastructure).
+                </li>
+                <li>New buildings have been added, or I have forgotten some.</li>
+            </ul>
+            <p>
+                Send me some feedback (button on top right) if you notice changed or missing buildings!
+                I&#39;ll add styles here if people request them and I have time :)
+            </p>
+            <p>
+                Viewing buildings at different levels is not supported yet, but I might add it in the future.
+                Currently the screenshots are of the maximum level of the building.
+            </p>
+        </article>
+    );
 }
