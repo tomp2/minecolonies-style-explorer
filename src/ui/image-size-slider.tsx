670e373c810383db8bc77e35014b88ac991984d8
tomp2 <51736857+tomp2@users.noreply.github.com>
2025-01-09 14:37:21 +0200

    feat: dynamic column count to keep card size same on window resize

--- src/ui/image-size-slider.tsx
+++ src/ui/image-size-slider.tsx
@@ -1,32 +1,95 @@
+import { useSidebar } from "@/components/ui/sidebar.tsx";
 import { Slider } from "@/components/ui/slider.tsx";
 import { useDelayedCaptureEvent } from "@/hooks/delayed-capture-event.ts";
+import { useIsMobile } from "@/hooks/use-mobile.tsx";
+import useWindowWidth from "@/hooks/use-window-width.ts";
+import { useEffect, useState } from "react";
 
-if (localStorage.getItem("imgsize") !== null) {
-    localStorage.removeItem("imgsize");
+const defaultColumnSize = 350;
+const minColumnSize = 130;
+const sidebarWidth = 256;
+
+function setCssColumns(value: number) {
+    document.documentElement.style.setProperty("--image-cols", value.toString());
 }
-const storedValue = localStorage.getItem("imgcols");
-const defaultImgSize = 350;
-const defaultColumns = Math.floor(window.innerWidth / defaultImgSize);
 
-const initialCols = storedValue ? Number.parseInt(storedValue) : defaultColumns;
-document.documentElement.style.setProperty("--image-cols", initialCols.toString());
+function writeConfig(columns: number, containerWidth: number) {
+    localStorage.setItem("imgcols", columns.toString());
+    localStorage.setItem("containerSize", containerWidth.toString());
+}
+
+function readConfig() {
+    const storedColumns = localStorage.getItem("imgcols");
+    const storedContainerWidth = localStorage.getItem("containerSize");
+    if (!storedColumns || !storedContainerWidth) return null;
+    return {
+        columns: Number.parseInt(storedColumns),
+        containerWidth: Number.parseInt(storedContainerWidth),
+    };
+}
+
+const initialStoredConfig = readConfig();
+
+let containerWidth = window.innerWidth;
+const isMobile = window.innerWidth < 768;
+if (!isMobile) containerWidth -= sidebarWidth;
+let initialColumns = Math.floor(containerWidth / defaultColumnSize);
+if (initialStoredConfig) {
+    initialColumns = Math.floor(
+        (initialStoredConfig.columns * containerWidth) / initialStoredConfig.containerWidth,
+    );
+}
+setCssColumns(initialColumns);
+
+function useContainerWidth() {
+    const windowWidth = useWindowWidth();
+    const sidebar = useSidebar();
+    const isMobile = useIsMobile();
+    let containerWidth = windowWidth;
+    if (sidebar.open && !isMobile) containerWidth -= sidebarWidth;
+    return containerWidth;
+}
 
 export function ImageSizeSlider() {
     const delayedCapture = useDelayedCaptureEvent();
+    const containerWidth = useContainerWidth();
+    const [sliderColumns, setSliderColumns] = useState(initialColumns);
+    const [storedColumns, setStoredColumns] = useState(initialStoredConfig?.columns);
+    const [storedContainerWidth, setStoredContainerWidth] = useState(initialStoredConfig?.containerWidth);
+
+    const maxColumns = Math.floor(containerWidth / minColumnSize);
+
+    function sliderColumnsChanged(columns: number) {
+        columns = Math.min(columns, maxColumns);
+        setSliderColumns(columns);
+        setCssColumns(columns);
+        writeConfig(columns, containerWidth);
+        setStoredColumns(columns);
+        setStoredContainerWidth(containerWidth);
+        delayedCapture(10_000, "image_cols_changed_v2", {
+            columns,
+            containerWidth,
+            imgSize: Math.floor(containerWidth / columns),
+        });
+    }
+
+    useEffect(() => {
+        if (storedColumns && storedContainerWidth) {
+            const columns = Math.floor((storedColumns * containerWidth) / storedContainerWidth);
+            setCssColumns(columns);
+            setSliderColumns(columns);
+        }
+    }, [containerWidth]);
 
     return (
         <div className="px-2">
             <Slider
-                defaultValue={[initialCols]}
+                value={[sliderColumns]}
                 aria-label="Number of columns for images"
                 min={1}
-                max={10}
+                max={maxColumns}
                 step={1}
-                onValueChange={([value]) => {
-                    localStorage.setItem("imgcols", value.toString());
-                    document.documentElement.style.setProperty("--image-cols", value.toString());
-                    delayedCapture(10_000, "image_cols_changed", { size: value });
-                }}
+                onValueChange={([value]) => sliderColumnsChanged(value)}
             />
         </div>
     );
